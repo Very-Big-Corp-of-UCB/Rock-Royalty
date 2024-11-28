e2abc3b8bff3032d7909b30e4f8fde7c8ef7cd61
emi-ly111 <emilyhgomez123@gmail.com>
2024-11-27 23:56:12 -0300

    style(modal): ajusta o padding e a margin do modal de cadastro.

--- script/script.js
+++ script/script.js
@@ -37,30 +37,3 @@ document.querySelectorAll(".zoom-image").forEach((image) => {
     });
   });
 });
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000);
-}
-
-function plusSlides(n) {
-  slideIndex += n;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  if (slideIndex < 1) {
-    slideIndex = slides.length;
-  }
-  showSlides();
-}
